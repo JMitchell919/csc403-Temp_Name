d62eddfc4bce814377b29e3a2ac676ca80767a28
ctm042 <ctm042@latech.edu>
2024-10-10 00:22:11 -0500

    can now POST to /posts api

--- web/src/js/post.js
+++ web/src/js/post.js
@@ -1,3 +1,5 @@
+// const { response } = require("express");
+
 // On document load
 document.addEventListener('DOMContentLoaded', function() {
     // Add click event to submit-post button
@@ -6,10 +8,50 @@ document.addEventListener('DOMContentLoaded', function() {
         const input = document.getElementById('post-input').value;
         console.log(input);
 
-        // Get data needed and send off to database
+        // Get data needed for post
+        // Fetch user details
+
+        // Construct post
+        post = {
+            id: 2,
+            username: 'user',
+            profilePic: '',
+            location: 'Ruston, Louisiana',
+            date: '2024-10-10',
+            text: input,
+            postPics: [],
+            likeCount: 0,
+            dislikeCount: 0,
+            commentCount: 0
+        }
+
+
+        // Send post off
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(post),
+        })
+        .then(response => {
+            if (!response.ok) {
+                // Alert user of sent post
+                alert("Network response was not ok");
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            
+        })
+        .catch(error => {
+            alert(error);
+            console.error('Oopsie poopsie', error);
+        });
 
         // Alert user of sent post
-        alert("Post sent!")
+        alert("Post sent!");
 
         // Redirect to home page after login
         window.location.href = '/';
