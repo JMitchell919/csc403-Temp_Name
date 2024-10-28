5ccd141123d72a96c7a679fd5ea31303733a25ad
Greendragon2170 <bladedancer2001@gmail.com>
2024-10-27 22:32:31 -0500

    Add files via upload

---
+++ web/src/js/confirm.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    document.getElementById('submitConfirmation').addEventListener('click', function() {
+        window.location.href = 'login.html'
+    });
+});   
+// 
+//<script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+//<script>
+//   emailjs.init("YOUR_USER_ID");  // Initialize with your EmailJS user ID
+//</script>                                                              
+//
