b8e4acfa5ed80dde827244b3eac5d5bebeb1b27b
Greendragon2170 <bladedancer2001@gmail.com>
2024-11-10 09:49:54 -0600

    Update users_db.sql

--- database/accounts/users_db.sql
+++ database/accounts/users_db.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE IF NOT EXISTS users_db;
+-- CREATE DATABASE IF NOT EXISTS users_db;
 
-USE users_db;
+-- USE users_db;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    profile_pic VARCHAR(255)
-);
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(255) NOT NULL,
+--     password VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     profile_pic VARCHAR(255)
+-- );
 
