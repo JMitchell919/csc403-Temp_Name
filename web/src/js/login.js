5aa8d744c901b7ccbb04ed1021d0132dfde3baa2
Greendragon2170 <bladedancer2001@gmail.com>
2024-11-10 09:23:43 -0600

    Update login.js

--- web/src/js/login.js
+++ web/src/js/login.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('submitLogin').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() { 
+    document.getElementById('submitLogin').addEventListener('click', function() { 
 
-        const username = document.getElementById('username-input').value;
-        const password = document.getElementById('password-input').value;
+        const username = document.getElementById('username-input').value; 
+        const password = document.getElementById('password-input').value; 
 
-        console.log(`${username} ${password}`)
+        console.log(`${username} ${password}`) 
 
-        // Simulate login validation (replace this with actual back-end validation)
-        if (username === 'user' && password === 'password') {
-            // Store log in in local storage for persistance
-            localStorage.setItem('isLoggedIn', 'true');
-            localStorage.setItem('username', username);
+        // Simulate login validation (replace this with actual back-end validation) 
+        if (username === 'user' && password === 'password') { 
+            // Store log in in local storage for persistance 
+            localStorage.setItem('isLoggedIn', 'true'); 
+            localStorage.setItem('username', username); 
 
-            // Redirect to home page after login
-            window.location.href = '/';
-        } else {
-            alert('Invalid username or password!');
-        }
-    });
-});
+            // Redirect to home page after login 
+            window.location.href = '/'; 
+        } else { 
+            alert('Invalid username or password!'); 
+        } 
+    }); 
+}); 
