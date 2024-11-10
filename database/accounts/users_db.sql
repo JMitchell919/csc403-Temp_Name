32eca8ad7d484c5a2bfc199c7bae15295f5382e3
Greendragon2170 <bladedancer2001@gmail.com>
2024-11-09 22:33:36 -0600

    Update users_db.sql

--- database/accounts/users_db.sql
+++ database/accounts/users_db.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     profile_pic VARCHAR(255)
 );
 
