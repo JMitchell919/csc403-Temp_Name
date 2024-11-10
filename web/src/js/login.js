ee55a48cc3a5374a731a84dac3ec5907613e40eb
Greendragon2170 <bladedancer2001@gmail.com>
2024-11-10 11:53:15 -0600

    Update login.js

--- web/src/js/login.js
+++ web/src/js/login.js
@@ -1,21 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() { 
-    document.getElementById('submitLogin').addEventListener('click', function() { 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('submitLogin').addEventListener('click', async function() {
+        
+        const username = document.getElementById('username-input').value;
+        const password = document.getElementById('password-input').value;
+        
+        const user = await fetchUser(username);
 
-        const username = document.getElementById('username-input').value; 
-        const password = document.getElementById('password-input').value; 
+        if (user) {
+            // Compare entered password with stored password
+            if (user.password === password) {
+                // Set login status in local storage
+                localStorage.setItem('isLoggedIn', 'true');
+                localStorage.setItem('username', username);
+                
+                // Redirect to home page
+                window.location.href = '/';
+            } else {
+                alert('Incorrect password. Please try again.');
+            }
+        } else {
+            alert('Username not found.');
+        }
+    });
 
-        console.log(`${username} ${password}`) 
+    async function fetchUser(username) {
+        let apiDomain = '';
+        let apiPort = '';
 
-        // Simulate login validation (replace this with actual back-end validation) 
-        if (username === 'user' && password === 'password') { 
-            // Store log in in local storage for persistance 
-            localStorage.setItem('isLoggedIn', 'true'); 
-            localStorage.setItem('username', username); 
+        await fetch('/config')
+            .then(response => response.json())
+            .then(config => {
+                apiDomain = config.apiDomain;
+                apiPort = config.apiPort;
+            })
+            .catch(error => {
+                console.error('Error fetching config:', error);
+            });
 
-            // Redirect to home page after login 
-            window.location.href = '/'; 
-        } else { 
-            alert('Invalid username or password!'); 
-        } 
-    }); 
-}); 
+        try {
+            const response = await fetch(`${apiDomain}:${apiPort}/getUser?username=${username}`, {
+                method: 'GET'
+            });
+
+            if (response.ok) {
+                return await response.json(); // Return the user data as JSON
+            } else if (response.status === 404) {
+                return null; // User does not exist
+            }
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        }
+    }
+});
