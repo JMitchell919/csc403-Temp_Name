40d79bafde5bdc229c4fc58da7bfe3cf99d11373
Greendragon2170 <bladedancer2001@gmail.com>
2024-11-12 18:49:08 -0600

    Add files via upload

---
+++ restservice/src/main/java/com/example/restservice/new/controllers/UserController.java
@@ -0,0 +1,59 @@
+package com.example.rest_service;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Get a single post's data. (/view page)
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> userData) {
+        String username = userData.get("username");
+        String password = userData.get("password");
+        String email = userData.get("email");
+
+        try {
+            userService.writeUser(username, password, email);
+            return ResponseEntity.ok("User created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Registration failed");
+        }
+    }
+    // Endpoint to fetch all user information by username
+    @GetMapping("/getUser")
+    public ResponseEntity<Map<String, Object>> getUser(@RequestParam String username) {
+        Map<String, Object> userData = userService.readUser(username);
+        if (userData != null) {
+            return ResponseEntity.ok(userData);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HashMap<>());
+        }
+    }
+
+    // Endpoint to fetch only username and profilePic by userID
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<Map<String, Object>> getUserInfo(@RequestParam int userId) {
+        Map<String, Object> userInfo = userService.readUserInfo(userId);
+        if (userInfo != null) {
+            return ResponseEntity.ok(userInfo);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HashMap<>());
+        }
+    }
+}
